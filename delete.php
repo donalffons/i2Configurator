833309751835924f29a0b009bb5d2912edb8f96e
Sebastian <donalffons@gmail.com>
2018-07-22 00:37:35 +0200

    Fix crunchdb paths

--- delete.php
+++ delete.php
@@ -1,11 +1,8 @@
 <?php
-    // Require source files (not needed if you use composer)
-    require 'crunchDB/src/crunchDB.php';
-    require 'crunchDB/src/crunchTable.php';
-    require 'crunchDB/src/crunchResource.php';
-    // Alias class accessor to omit namespace (optional)
+    require __DIR__.'/crunchdb/src/crunchDB.php';
+    require __DIR__.'/crunchdb/src/crunchTable.php';
+    require __DIR__.'/crunchdb/src/crunchResource.php';
     use cybrox\crunchdb\CrunchDB as CrunchDB;
-    // Creating new cdb instance
 
     $dbdir = "WebGL Models/".urldecode($_POST["modelFolder"])."/db/";
     if (!file_exists($dbdir)) {
