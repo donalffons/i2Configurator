fb964c2801f347e12efe41d3709707f4a83113ca
Sebastian <donalffons@gmail.com>
2018-07-24 00:41:16 +0200

    More verbose logging

--- tests/testpuppeteer.js
+++ tests/testpuppeteer.js
@@ -26,7 +26,8 @@ exports.testAdding = async function(test){
             const page = await browser.newPage();
             await page.goto('http://127.0.0.1:3000/explorer.php', {waitUntil: 'networkidle0'});
             bodyHTML = await page.evaluate(() => document.body.innerHTML);
-            await console.log("Login\n");
+            console.log(bodyHTML);
+            await console.log("-----Login-----\n");
             // Login
             await page.type('#fm_usr', 'admin');
             await page.type('#fm_pwd', 'admin');
@@ -34,31 +35,41 @@ exports.testAdding = async function(test){
                 page.click("input[type=submit]"),
                 page.waitForNavigation({ waitUntil: 'networkidle0' }),
             ]);
-            await console.log("Click on Dancing Robot Test\n");
+            bodyHTML = await page.evaluate(() => document.body.innerHTML);
+            console.log(bodyHTML);
+            await console.log("-----Click on Dancing Robot Test-----\n");
             // Click on Dancing Robot Test
             await Promise.all([
                 page.click('a[href="?p=Dancing+Robot+Test"]'),
                 page.waitForNavigation({ waitUntil: 'networkidle0' }),
             ]);
-            await console.log("Click on Edit link\n");
+            bodyHTML = await page.evaluate(() => document.body.innerHTML);
+            console.log(bodyHTML);
+            await console.log("-----Click on Edit link-----\n");
             // Click on Edit link
             await Promise.all([
                 page.click('a[href="http://localhost/I2Configurator/editor.html?model=Dancing%20Robot%20Test&variant=No%20Dancing%20Dark%20Blue%20-%20Title"]'),
                 page.waitForNavigation({ waitUntil: 'networkidle0' }),
             ]);
-            await console.log("Hover on File\n");
+            bodyHTML = await page.evaluate(() => document.body.innerHTML);
+            console.log(bodyHTML);
+            await console.log("-----Hover on File-----\n");
             // Hover on File
             await Promise.all([
                 page.hover('div[innerHTML="File"]'),
                 page.waitForNavigation({ waitUntil: 'networkidle0' }),
             ]);
-            await console.log("Click on Close\n");
+            bodyHTML = await page.evaluate(() => document.body.innerHTML);
+            console.log(bodyHTML);
+            await console.log("-----Click on Close-----\n");
             // Click on Close
             await Promise.all([
                 page.click('div[innerHTML="Close"]'),
                 page.waitForNavigation({ waitUntil: 'networkidle0' }),
             ]);
-            await console.log("browser.close()\n");
+            bodyHTML = await page.evaluate(() => document.body.innerHTML);
+            console.log(bodyHTML);
+            await console.log("-----browser.close()-----\n");
             await browser.close();
         })();
 
