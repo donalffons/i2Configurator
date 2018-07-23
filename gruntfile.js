24011387fb2d9efdded3a42cbd6a1f08ca0c990f
Sebastian <donalffons@gmail.com>
2018-07-23 22:20:07 +0200

    async / await is currently not supported by JSHint (and therefore by grunt-jshint). https://github.com/jshint/jshint/pull/3273 Remove JSHint support for now

--- gruntfile.js
+++ gruntfile.js
@@ -1,14 +1,13 @@
 module.exports = function(grunt) {
   grunt.initConfig({
-    jshint: {
-      all: ['gruntfile.js', 'tests/**/*.js']
-    },
+    //jshint: {
+    //  all: ['gruntfile.js', 'tests/**/*.js']
+    //},
     nodeunit: {
       all: ['tests/**/*.js']
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
   grunt.registerTask('test', ['jshint', 'nodeunit']);
