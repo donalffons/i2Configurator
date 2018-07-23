d3fcc4164d137f1181f3b36f50e82e6dc7ef44a7
Sebastian <donalffons@gmail.com>
2018-07-23 23:02:07 +0200

    some more output logging

--- tests/testpuppeteer.js
+++ tests/testpuppeteer.js
@@ -21,17 +21,19 @@ exports.testAdding = async function(test){
         console.log('waited 3 seconds\n');
 
         await (async () => {
-            const browser = await puppeteer.launch()
-            const page = await browser.newPage()
-            await page.goto('http://127.0.0.1:3000/explorer.php')
-            await page.type('#fm_usr', 'admin')
-            await page.type('#fm_pwd', 'admin')
-            await page.click(':nth-child(2) > :nth-child(2) > .filename > a')
-            await page.goto('http://localhost/I2Configurator/explorer.php?p=Dancing+Robot+Test')
-            await page.click(':nth-child(2) > :nth-child(3) > a')
-            await page.goto('http://localhost/I2Configurator/editor.html?model=Dancing%20Robot%20Test&variant=No%20Dancing%20Dark%20Blue%20-%20Title')
-            await page.click(':nth-child(2) > :nth-child(2) > a')
-            await page.goto('http://localhost/I2Configurator/viewer.html?model=Dancing%20Robot%20Test&variant=No%20Dancing%20Dark%20Blue%20-%20Title')
+            const browser = await puppeteer.launch();
+            const page = await browser.newPage();
+            await page.goto('http://127.0.0.1:3000/explorer.php');
+            let bodyHTML = await page.evaluate(() => document.body.innerHTML);
+            console.log("Page content: \n"+bodyHTML);
+            await page.type('#fm_usr', 'admin');
+            await page.type('#fm_pwd', 'admin');
+            await page.click(':nth-child(2) > :nth-child(2) > .filename > a');
+            await page.goto('http://localhost/I2Configurator/explorer.php?p=Dancing+Robot+Test');
+            await page.click(':nth-child(2) > :nth-child(3) > a');
+            await page.goto('http://localhost/I2Configurator/editor.html?model=Dancing%20Robot%20Test&variant=No%20Dancing%20Dark%20Blue%20-%20Title');
+            await page.click(':nth-child(2) > :nth-child(2) > a');
+            await page.goto('http://localhost/I2Configurator/viewer.html?model=Dancing%20Robot%20Test&variant=No%20Dancing%20Dark%20Blue%20-%20Title');
             await browser.close()
         })()
 
