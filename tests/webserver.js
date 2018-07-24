99e0983ccfc3fc46de1dd93325059265e038e68f
Sebastian <donalffons@gmail.com>
2018-07-24 02:00:15 +0200

    attempting to fix grunt-php server

--- tests/webserver.js
+++ tests/webserver.js
@@ -1,15 +1,18 @@
 /*jshint esversion: 6 */
 
-grunt.initConfig({
-    php: {
-        test: {
-            options: {
-                port: 3000,
-                keepalive: true
+module.exports = function(grunt) {
+    grunt.initConfig({
+        php: {
+            test: {
+                options: {
+                    port: 3000,
+                    keepalive: true
+                }
             }
         }
-    }
-});
+    });
 
-grunt.registerTask('server', ['php']);
-console.log("Started PHP webserver on port 3000.\n")
+    grunt.loadNpmTasks('grunt-php');
+    grunt.registerTask('server', ['php']);
+    console.log("Started PHP webserver on port 3000.\n");
+}
