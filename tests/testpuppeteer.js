8993a5d62d0888f5239906823e30f124908e08fd
Sebastian <donalffons@gmail.com>
2018-07-24 21:29:52 +0200

    polished testpuppeteer.js

--- tests/testpuppeteer.js
+++ tests/testpuppeteer.js
@@ -6,69 +6,56 @@ var request = require('request');
 const puppeteer = require('puppeteer');
 const delay = require('delay');
 
-exports.testAdding = async function(test){  
-    var x = 3,
-        y = 2;
-    test.equals(x+y, 5, "Test add function");
+exports.testAdding = async function(test){
+    console.log("Waiting 3 seconds for server to load up...\n");
+    await delay(3000);
+    console.log('Continuing...\n');
 
-    var cp = childProcess.fork(path.join(__dirname, "/webserver.js"));
-    cp.on("exit", function (code, signal) {
-        console.log("Exited", {code: code, signal: signal});
+    const browser = await puppeteer.launch({devtools: true});
+    const page = await browser.newPage();
+    page.setDefaultNavigationTimeout(1000*60*5);
+    page.on('console', function(msg) { console.log('browser console: ', msg.text()); });
+    page.on("pageerror", function(err) { console.log("browser page error: " + err.toString()); test.ok(false); });
+    page.on("error", function(err) { console.log("browser error: " + err.toString()); test.ok(false); });
+    await page.goto('http://127.0.0.1:3000/explorer.php', {waitUntil: 'networkidle2'});
+
+    // Login
+    await console.log("-----Login-----\n");
+    await page.type('#fm_usr', 'admin');
+    await page.type('#fm_pwd', 'admin');
+    page.$eval('form', form => form.submit());
+    await page.waitForNavigation({ waitUntil: 'networkidle2' });
+    
+    // Click on Dancing Robot Test
+    await console.log("-----Click on Dancing Robot Test-----\n");
+    await Promise.all([
+        page.click('a[href="?p=Dancing+Robot+Test"]'),
+        page.waitForNavigation({ waitUntil: 'networkidle2' }),
+    ]);
+
+    // Click on Edit link
+    await console.log("-----Click on Edit link-----\n");
+    await Promise.all([
+        page.click('a[href="editor.html?model=Dancing%20Robot%20Test&variant=No%20Dancing%20Dark%20Blue%20-%20Title"]'),
+        page.waitForNavigation({ waitUntil: 'networkidle2' }),
+    ]);
+
+    // Hover on File
+    await console.log("-----Hover on File-----\n");
+    await (await page.$x('//div[normalize-space()="File"]'))[0].hover();
+
+    // Confirm Warning
+    page.on("dialog", (dialog) => {
+        console.log("-----Confirm warning dialog-----\n");
+        dialog.accept();
     });
-    cp.on("error", console.error.bind(console));
 
-    console.log("starting timeout\n");
-    setTimeout(async function() {
-        console.log('waited 3 seconds\n');
+    // Click on Close
+    await console.log("-----Click on Close-----\n");
+    (await page.$x('//div[normalize-space()="Close"]'))[0].click();
+    await page.waitForNavigation({ waitUntil: 'networkidle2' });
 
-        await (async () => {
-            const browser = await puppeteer.launch();
-            const page = await browser.newPage();
-            await page.goto('http://127.0.0.1:3000/explorer.php', {waitUntil: 'networkidle0'});
-
-            // Login
-            await console.log("-----Login-----\n");
-            await page.type('#fm_usr', 'admin');
-            await page.type('#fm_pwd', 'admin');
-            page.$eval('form', form => form.submit());
-            await page.waitForNavigation({ waitUntil: 'networkidle2' });
-            
-            // Click on Dancing Robot Test
-            await console.log("-----Click on Dancing Robot Test-----\n");
-            await Promise.all([
-                page.click('a[href="?p=Dancing+Robot+Test"]'),
-                page.waitForNavigation({ waitUntil: 'networkidle2' }),
-            ]);
-
-            // Click on Edit link
-            await console.log("-----Click on Edit link-----\n");
-            await Promise.all([
-                page.click('a[href="editor.html?model=Dancing%20Robot%20Test&variant=No%20Dancing%20Dark%20Blue%20-%20Title"]'),
-                page.waitForNavigation({ waitUntil: 'networkidle2' }),
-            ]);
-
-            // Hover on File
-            await console.log("-----Hover on File-----\n");
-            await (await page.$x('//div[normalize-space()="File"]'))[0].hover();
-
-            // Confirm Warning
-            page.on("dialog", (dialog) => {
-                console.log("-----Confirm warning dialog-----\n");
-                dialog.accept();
-            });
-
-            // Click on Close
-            await console.log("-----Click on Close-----\n");
-            (await page.$x('//div[normalize-space()="Close"]'))[0].click();
-            await page.waitForNavigation({ waitUntil: 'networkidle2' });
-
-            await console.log("-----browser.close()-----\n");
-            await browser.close();
-        })();
-
-        console.log('waited 3 seconds\n');
-
-        cp.kill('SIGINT');
-        test.done();
-    }, 3000);
+    await console.log("-----Finish Testing-----\n");
+    await browser.close();
+    test.done();
 };
