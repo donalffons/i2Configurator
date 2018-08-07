0c9839be9cba57fe69b675cbcf0a2f59a44021aa
Sebastian <donalffons@gmail.com>
2018-08-08 00:54:06 +0200

    more bugfixing

--- deploy/beforeinstall.sh
+++ deploy/beforeinstall.sh
@@ -1,4 +1,12 @@
 cd /var/www/html/
+
+# disable errors
+exec 3>&2
+exec 2> /dev/null
+
 mv "i2configurator/WebGL Models" "./WebGL Models"
 rm ./i2configurator -f -R
 mv "./WebGL Models" "./i2configurator/WebGL Models"
+
+# re-enable errors
+exec 2>&3
