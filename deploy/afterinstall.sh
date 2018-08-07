028e24a02dd6e8ffcd0d96ff1df0ad79b4488226
Sebastian <donalffons@gmail.com>
2018-08-08 00:36:40 +0200

    more work on automatic deployment

--- deploy/afterinstall.sh
+++ deploy/afterinstall.sh
@@ -1,2 +1,6 @@
-#!/bin/bash
-touch testfile.txt
+cd /var/www/html/i2configurator
+
+git clone https://github.com/mrdoob/three.js.git --branch master
+git clone https://github.com/donalffons/I2Configurator-Explorer.git --branch master
+
+chown apache:apache . -R
---
+++ deploy/beforeinstall.sh
@@ -0,0 +1,4 @@
+cd /var/www/html/
+mv "i2configurator/WebGL Models" .
+rm i2configurator -f -R
+mv "WebGL Models" i2configurator
