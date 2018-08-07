83281ec7a5ec4aabafcf42b28a6bf4180c7ff1d2
Sebastian <donalffons@gmail.com>
2018-08-08 00:45:48 +0200

    bux fixing for automatic deployment

--- deploy/beforeinstall.sh
+++ deploy/beforeinstall.sh
@@ -1,4 +1,4 @@
 cd /var/www/html/
-mv "i2configurator/WebGL Models" .
-rm i2configurator -f -R
-mv "WebGL Models" i2configurator
+mv "i2configurator/WebGL Models" "./WebGL Models"
+rm ./i2configurator -f -R
+mv "./WebGL Models" "./i2configurator/WebGL Models"
