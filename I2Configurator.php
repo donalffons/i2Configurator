2ae0039eec50b2a989242d362351a5d6eda81629
Sebastian <donalffons@gmail.com>
2018-08-08 11:34:19 +0200

    add i2actions table

--- I2Configurator.php
+++ I2Configurator.php
@@ -12,7 +12,8 @@ if(isset($_POST["api"])) {
     $conn->select_db("i2configurator");
     $conn->set_charset("utf8");
     $result = $conn->query("CREATE TABLE IF NOT EXISTS `i2models` ( `id` INT NOT NULL AUTO_INCREMENT , `name` TEXT NOT NULL , `path` TEXT NOT NULL , PRIMARY KEY (`id`)) ENGINE = MyISAM;");
-    $result = $conn->query("CREATE TABLE IF NOT EXISTS `i2variants` ( `id` INT NOT NULL AUTO_INCREMENT , `id model` TEXT NOT NULL, `action` TEXT NOT NULL , `name` TEXT NOT NULL , PRIMARY KEY (`id`)) ENGINE = MyISAM;");
+    $result = $conn->query("CREATE TABLE IF NOT EXISTS `i2variants` ( `id` INT NOT NULL AUTO_INCREMENT , `id model` INT NOT NULL, `action` TEXT NOT NULL , `name` TEXT NOT NULL , PRIMARY KEY (`id`)) ENGINE = MyISAM;");
+    $result = $conn->query("CREATE TABLE IF NOT EXISTS `i2actions` ( `id` INT NOT NULL AUTO_INCREMENT , `id variant` INT NOT NULL, `action` TEXT NOT NULL , `name` TEXT NOT NULL , PRIMARY KEY (`id`)) ENGINE = MyISAM;");
     header("Content-type: text/html;charset=utf-8");
     
     function getModelByPath($conn, $path) {
