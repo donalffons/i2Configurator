c81f1b7ee1268ad439c5ce408181f20cc06306c5
Sebastian <donalffons@gmail.com>
2018-08-08 02:17:51 +0200

    providing default configuration file

---
+++ I2Configurator-conf.php
@@ -0,0 +1,13 @@
+<?php
+
+/** MySQL database username */
+define('DB_USER', '???');
+
+/** MySQL database password */
+define('DB_PASSWORD', '???');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
+
+?>
+
