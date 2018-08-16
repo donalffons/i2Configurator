153a96d11952c5e6664205996cc9d9a0fedcfa31
Sebastian <donalffons@gmail.com>
2018-08-16 16:57:16 +0200

    updated i2ConfiguratorCore, updated builds

--- explorer.php
+++ explorer.php
@@ -3847,7 +3847,7 @@ f00bar;
 				<meta charset="utf-8">
 				<meta http-equiv="X-UA-Compatible" content="IE=edge">
 				<meta name="viewport" content="width=device-width, initial-scale=1">
-				<script src="i2ConfiguratorCore/build/i2ConfiguratorCore.js"></script>';
+				<script src="i2ConfiguratorCore/build/i2ConfiguratorCore.min.js"></script>';
 		$this->getCSS();
 		print '</head><body>';
 	}
